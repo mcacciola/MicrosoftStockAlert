668e64a0f701492ab5d1287ea287019bdf761d88
Cacciola, Mitchel (Non-Employee) <mitchel.cacciola@firstdata.com>
2016-09-20 14:28:36 -0400

    added dummy test

---
+++ StockAlert/src/test/java/StockAlertTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StockAlertTest {
+
+    @Test
+    public void canCreateStockAlert(){
+        assertEquals(StockAlert.class, new StockAlert().getClass());
+    }
+
+}
