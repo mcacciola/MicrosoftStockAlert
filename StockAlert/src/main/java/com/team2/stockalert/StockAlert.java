d3cbd52fdd46603345ca07108f4e4e691ff2478c
Cacciola, Mitchel (Non-Employee) <mitchel.cacciola@firstdata.com>
2016-09-21 11:25:31 -0400

    Added first test for stock quote. Removed .idea workspace folder from project.

--- StockAlert/src/main/java/com/team2/stockalert/StockAlert.java
+++ StockAlert/src/main/java/com/team2/stockalert/StockAlert.java
@@ -4,4 +4,7 @@ package com.team2.stockalert;
  * Created by f563oyy on 9/20/2016.
  */
 public class StockAlert {
+    public static int getStockQuote() {
+        return 53;
+    }
 }
--- StockAlert/src/test/java/com/team2/stockalert/StockAlertTest.java
+++ StockAlert/src/test/java/com/team2/stockalert/StockAlertTest.java
@@ -11,4 +11,9 @@ public class StockAlertTest {
         assertEquals(StockAlert.class, new StockAlert().getClass());
     }
 
+    @Test
+    public void stockQuoteIs53(){
+        assertEquals(53, StockAlert.getStockQuote());
+    }
+
 }
